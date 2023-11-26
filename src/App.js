407e66a0643dd203177509a4b612a27c35c06fe9
Harshali Kanoje <harshalikanoje@gmail.com>
2023-11-26 09:08:25 -0800

    build a tic tac toe game

--- src/App.js
+++ src/App.js
@@ -1,25 +1,153 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react'
+import './App.css'
 
-function App() {
+const App = () => {
+
+  const [player,setPlayer] = useState(1);
+  const [winner,setWinner] = useState(null);
+  const [board,setBoard] = useState({
+    1 : "",
+    2 : "",
+    3 : "",
+    4 : "",
+    5 : "",
+    6 : "",
+    7 : "",
+    8 : "",
+    9 : "",
+  });
+   
+  const checkWinner = () => {
+    const symbol = player === 1 ? "🙋‍♀️" : "🙋‍♂️";
+    if(board[1] === symbol && board[2] === symbol && board[3] === symbol)
+    {
+      setWinner(player)
+    } 
+    else if(board[4] === symbol && board[5] === symbol && board[6] === symbol)
+    {
+      setWinner(player)
+    }
+    else if(board[7] === symbol && board[8] === symbol && board[9] === symbol)
+    {
+      setWinner(player)
+    }
+    else if(board[1] === symbol && board[4] === symbol && board[7] === symbol)
+    {
+      setWinner(player)
+    }
+    else if(board[2] === symbol && board[5] === symbol && board[8] === symbol)
+    {
+      setWinner(player)
+    }
+    else if(board[3] === symbol && board[6] === symbol && board[9] === symbol)
+    {
+      setWinner(player)
+    }
+    else if(board[1] === symbol && board[5] === symbol && board[9] === symbol)
+    {
+      setWinner(player)
+    }
+    else if(board[3] === symbol && board[5] === symbol && board[7] === symbol)
+    {
+      setWinner(player)
+    }
+    setPlayer(player === 1 ? 2 : 1)
+  }
+
+  const play = (boxNo) => 
+  {
+    if(board[boxNo] !== "" || winner != null)
+    {
+      return;
+    }
+
+    if(player === 1)
+    {
+      setBoard({...board,[boxNo]:"🙋‍♀️"})
+    }
+    else
+    {
+      setBoard({...board,[boxNo]:"🙋‍♂️"})
+    }
+
+    // checkWinner(player)
+    // setPlayer(player === 1 ? 2 : 1)
+  }
+
+  useEffect(() => {
+    checkWinner()
+  },[board])
+
+  const reset = () => {
+    setPlayer(1);
+    setBoard({
+    1 : "",
+    2 : "",
+    3 : "",
+    4 : "",
+    5 : "",
+    6 : "",
+    7 : "",
+    8 : "",
+    9 : "",
+    })
+    setWinner(null)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 className='text-center'> Tic Tac Toe</h1>
+      <div className='player-container'>
+        <span> Player 1 🙋‍♀️</span>
+        <span> Player 2 🙋‍♂️</span>
+      </div>
+      <div>
+        <p className='text-center'>Current Player : {player === 1?"🙋‍♀️":"🙋‍♂️"}</p>
+      </div>
+      <div className='board'>
+        <div className='row'>
+          <div className='box' onClick={() => {play(1)}}>
+            {board[1]}
+          </div>
+          <div className='box' onClick={() => {play(2)}}>
+          {board[2]}
+          </div>
+          <div className='box' onClick={() => {play(3)}}>
+          {board[3]}
+          </div>
+        </div>
+
+        <div className='row'>
+          <div className='box' onClick={() => {play(4)}}>
+          {board[4]}
+          </div>
+          <div className='box' onClick={() => {play(5)}}>
+          {board[5]}
+          </div>
+          <div className='box' onClick={() => {play(6)}}>
+          {board[6]}
+          </div>
+        </div>
+
+        <div className='row'>
+          <div className='box' onClick={() => {play(7)}}>
+          {board[7]}
+          </div>
+          <div className='box'onClick={() => {play(8)}}>
+          {board[8]}
+          </div>
+          <div className='box' onClick={() => {play(9)}}>
+          {board[9]}
+          </div>
+        </div>
+      </div>
+
+      {winner ? <h1>Winner is : {winner === 1?"🙋‍♀️":"🙋‍♂️"}</h1>: null }
+      <button type='button' onClick={() =>{
+        reset()
+      }}>Reset</button>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
